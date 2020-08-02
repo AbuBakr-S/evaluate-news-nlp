7a243633f218ffc115c7c4ddd0930c2e4cafac5e
Abubakar Siddique <abz.siddique@gmail.com>
2020-08-02 14:43:42 +0100

    chore: Install cors, bodyparser and dotenv

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 
 const app = express()
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static('dist'))
 
