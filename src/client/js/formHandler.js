c95fd593d4559a367c1d3fc4313a9486f1fc263a
Abubakar Siddique <abz.siddique@gmail.com>
2020-08-02 17:11:24 +0100

    test: Add console log to confirm form input

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,16 +3,15 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('analyse-text').value
-    // Example text provided by the semantic analysis api
-    let example = "Main%20dishes%20were%20quite%20good%2C%20but%20desserts%20were%20too%20sweet%20for%20me."
 
     console.log("::: Form Submitted :::")
     // TODO: API_KEY reference to env variable is currently not working. This needs to be fixed
-    fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&of=json&txt=${example}&model=general&lang=en`)
+    fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&of=json&txt=${formText}&model=general&lang=en`)
     .then(res => res.json())
     .then(function(res) {
         // This is the confidence rating by semantic analysis api
         document.getElementById('results').innerHTML = res.confidence
+        console.log(formText)
     })
 }
 
