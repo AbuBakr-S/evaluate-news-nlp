0e4983313657444779d237842f107633b4254220
Abubakar Siddique <abz.siddique@gmail.com>
2020-08-08 12:12:58 +0100

    feat: Create a test route
    Moving fetch request with api key from client to server side

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-var path = require('path')
-const express = require('express')
+var path = require('path');
+const express = require('express');
+var axios = require("axios");
 
-const app = express()
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -16,16 +17,33 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-console.log(__dirname)
+console.log(__dirname);
 
 // Home route pointing to index.html
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
+    res.sendFile('dist/index.html');
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+    console.log('Example app listening on port 8081!');
+})
+
+app.get("/test", function (req, res){
+    let projectData = {};
+    const name = req.query.name;
+    axios.post(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&of=json&txt=${name}&model=general&lang=en`, {})
+    .then(function (response){
+        const result = response.data; 
+        projectData["sentence"] = name;
+        projectData["subjectivity"]= result.subjectivity; 
+        res.json(projectData);
+        res.end();
+    }) 
+    .catch (function (error) {
+        console.log(error);
+        res.end();
+    })
 })
