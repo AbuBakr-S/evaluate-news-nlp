8bdf729ab7c79c87a0987f38eb7db6a9fce8d273
Abubakar Siddique <abz.siddique@gmail.com>
2020-09-11 19:10:59 +0100

    feat: Attempt to add images to dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -23,6 +23,12 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                  'file-loader',
+                ],
             }
         ]
     },
