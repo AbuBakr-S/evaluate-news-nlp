cf8c2d05a204c07bfd32f562eefbdad637440024
Abubakar Siddique <abz.siddique@gmail.com>
2020-09-19 12:13:08 +0100

    chore: Initialise webpack-icons-installer

--- webpack.dev.js
+++ webpack.dev.js
@@ -22,6 +22,15 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                options: {
+                  limit: 8192,
+                  name:'[name].[ext]',
+                  outputPath:'assets' //the icons will be stored in dist/assets folder
+                }
             }
         ]
     },
--- webpack.prod.js
+++ webpack.prod.js
@@ -26,6 +26,15 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                options: {
+                  limit: 8192,
+                  name:'[name].[ext]',
+                  outputPath:'assets' //the icons will be stored in dist/assets folder
+                }   
             }
         ]
     },
