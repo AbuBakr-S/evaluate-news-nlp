435e0914d50dae86c1e66269b78d6b191e3d0f41
Abubakar Siddique <abz.siddique@gmail.com>
2020-09-27 20:46:21 +0100

    test: Initialising tests for all 3 js files

--- __test__/formCharLimit.test.js
+++ __test__/formCharLimit.test.js
@@ -1,11 +1,11 @@
 // 1. Import the js file to test
-import { handleSubmit } from "../src/client/js/formHandler"
+import { charCountUpdate } from "../src/client/js/formCharLimit"
 
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.  
 // A test suite may contain one or more related tests    
-describe("Testing the submit functionality", () => {
+describe("Testing the formCharLimit length", () => {
     // The test() function has two arguments - a string description, and an actual test as a callback function.  
-    test("The handleSubmit() function successfully returns data from the API", () => {
+    test("The ormCharLimit length is more than or equal to 0", () => {
         // 2. Define the input for the function, if any, in the form of variables/array
         
 
@@ -15,6 +15,6 @@ describe("Testing the submit functionality", () => {
         // 4. The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
         // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
 
-        expect(handleSubmit).toBeDefined();
+        expect(length).toBeGreaterThanOrEqual(0);
     })
 });
--- __test__/formInputValidation.test.js
+++ __test__/formInputValidation.test.js
@@ -1,11 +1,11 @@
 // 1. Import the js file to test
-import { handleSubmit } from "../src/client/js/formHandler"
+import { formInputValidation } from "../src/client/js/formInputValidation"
 
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.  
 // A test suite may contain one or more related tests    
-describe("Testing the submit functionality", () => {
+describe("Testing the REGEX check for numbers only form input data", () => {
     // The test() function has two arguments - a string description, and an actual test as a callback function.  
-    test("The handleSubmit() function successfully returns data from the API", () => {
+    test("The formInputValidation function should not contain numbers only", () => {
         // 2. Define the input for the function, if any, in the form of variables/array
         
 
@@ -15,6 +15,6 @@ describe("Testing the submit functionality", () => {
         // 4. The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
         // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
 
-        expect(handleSubmit).toBeDefined();
+        expect('12345').toMatch(/(^[0-9]+$)/);
     })
 });
