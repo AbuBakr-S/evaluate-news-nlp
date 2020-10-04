d36367622cafab05a0596338a16cda836ee3111d
Abubakar Siddique <abz.siddique@gmail.com>
2020-10-04 18:09:53 +0100

    Revert "refactor: Reorder css imports"
    This reverts commit 0c0deebc92b495628be1931f89354cf4cc7ee587.

--- src/client/index.js
+++ src/client/index.js
@@ -4,10 +4,9 @@ import { inputValidation } from './js/formInputValidation'
  
 import './styles/resets.scss'
 import './styles/base.scss'
-import './styles/header.scss'
-import './styles/form.scss'
 import './styles/footer.scss'
-
+import './styles/form.scss'
+import './styles/header.scss'
 
 export { handleSubmit }
 export { charCountUpdate }
