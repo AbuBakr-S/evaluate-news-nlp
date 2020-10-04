b8f5d365f9ae0d57a0843c7386e6d99a7c405e86
Abubakar Siddique <abz.siddique@gmail.com>
2020-10-04 18:30:38 +0100

    refactor: Reorder style imports

--- src/client/index.js
+++ src/client/index.js
@@ -4,9 +4,9 @@ import { inputValidation } from './js/formInputValidation'
  
 import './styles/resets.scss'
 import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
 import './styles/header.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
 
 export { handleSubmit }
 export { charCountUpdate }
